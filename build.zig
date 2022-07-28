32282ab4e4385bc7016d9e68f258c5886d68ddba
Ultra-Code <mega.alpha100@gmail.com>
2022-07-28 00:07:27 +0000

    Add build.zig for a mixed c/zig project

---
+++ build.zig
@@ -0,0 +1,65 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) !void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard release options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    const mode = b.standardReleaseOptions();
+
+    const cflags = [_][]const u8{
+        "-std=c2x", "-Wall", "-Wextra", "-Weverything", "-Werror", //
+        "-pedantic", "-pedantic-errors", //
+        "-Wno-declaration-after-statement", "-Wno-unused-function", //
+    };
+
+    const exe = b.addExecutable("relearn", "src/main.zig");
+    var sources = std.ArrayList([]const u8).init(b.allocator);
+
+    // Search for all C/C++ files in `src` and add them
+    {
+        var dir = try std.fs.cwd().openIterableDir("src", .{});
+
+        var walker = try dir.walk(b.allocator);
+        defer walker.deinit();
+
+        const allowed_exts = [_][]const u8{ ".c", ".cpp", ".cxx", ".c++", ".cc" };
+        while (try walker.next()) |entry| {
+            const ext = std.fs.path.extension(entry.basename);
+            const include_file = for (allowed_exts) |e| {
+                if (std.mem.eql(u8, ext, e))
+                    break true;
+            } else false;
+            if (include_file) {
+                // we have to clone the path as walker.next() or walker.deinit() will override/kill it
+                try sources.append(try std.fmt.allocPrint(b.allocator, "src/{s}", .{entry.path}));
+            }
+        }
+    }
+
+    exe.addCSourceFiles(sources.items, &cflags);
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.linkLibC();
+    exe.install();
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const exe_tests = b.addTest("src/main.zig");
+    exe_tests.setTarget(target);
+    exe_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&exe_tests.step);
+}

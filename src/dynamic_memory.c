27cb1db7856a7ac51d4cd1ae0202614d062e4133
Ultra-Code <mega.alpha100@gmail.com>
2024-04-25 18:42:17 +0000

    Add implementation for circular
    Use standard C23 parameter for the struct with flex array field
    
    clang-tidy: remove const in declaration warning

--- src/dynamic_memory.c
+++ src/dynamic_memory.c
@@ -1,8 +1,68 @@
 #include "dynamic_memory.h"
+#include <assert.h>
+#include <err.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+circular *circular_init(circular *circle, size_t max_len) {
+  assert(max_len > 0);
+
+  *circle = (circular){
+      .max_len = max_len,
+  };
+  return circle;
+}
+
+void circular_destroy(circular *circle) { free(circle); }
+
+static size_t circular_getpos(circular const *const circle, size_t pos) {
+  pos += circle->start;
+  pos %= circle->max_len;
+  return pos;
+}
+
+double *circular_element(circular *const circle, size_t pos) {
+  assert(pos < circle->max_len);
+
+  pos = circular_getpos(circle, pos);
+  return &circle->tab[pos];
+}
+
+circular *circular_resize(circular *circle, size_t new_max_len) {
+  assert(new_max_len > 0 && circle->max_len < new_max_len);
+  assert(new_max_len != circle->max_len);
+
+  size_t ostart = circular_getpos(circle, 0UL);
+  size_t nstart = ostart;
+  // double *otab = circle->tab;
+  size_t len = circle->len;
+  circular *resized_circle =
+      realloc(circle, sizeof(circular) + sizeof(double[new_max_len]));
+  if (!resized_circle) {
+    err(EXIT_FAILURE, "%s", "couldn't resize the existing circle");
+  }
+  // TODO(verify-two-separate-chucks): verify whether you need the 2 separate
+  // segments fix
+  //  add content to the flex array and view the behaviour of realloc on the
+  //  already existing array
+  *resized_circle = (circular){
+      .max_len = new_max_len, .start = nstart, .len = len,
+      // .tab = {resized_circle->tab},
+  };
+  circle = resized_circle;
+  return circle;
+}
+
+circular *circular_new(size_t len) {
+  circular *new_circle = calloc(1UL, sizeof(circular) + sizeof(double[len]));
+  if (!new_circle) {
+    err(EXIT_FAILURE, "%s", "couldn't allocate a new circle");
+  }
+  return circular_init(new_circle, len);
+}
 
 int main() {
-  size_t length = 10;
+  size_t const length = {10};
   double *array = nullptr;
   printf("size of malloc is %zu\n", sizeof(typeof(array)[length]));
 }
--- src/dynamic_memory.h
+++ src/dynamic_memory.h
@@ -1,5 +1,8 @@
-#pragma once
+#ifndef DYNAMIC_MEMORY_H
+#define DYNAMIC_MEMORY_H
+
 #include <stddef.h>
+
 // circular: a type for a circular buffer for double values
 // This data structure allows to add double values in rear and to take them out
 // in front. Each such structure has a maximal amount of elements that can be
@@ -10,45 +13,64 @@ typedef struct circular {
   size_t start;   /**< Position of element 0 */
   size_t len;     /**< Number of elements stored */
   size_t max_len; /**< Maximum capacity */
-  // double tab[] [[clang::counted_by(len)]]; /**< Array holding the data*/
-  double tab[] __attribute__((counted_by(len)));
+  /**< Array holding the data*/
+  double tab[] __attribute__((counted_by(max_len)));
 } circular;
 
 // circular_append: Append a new element with value value to the buffer c.
 // Returns: c if the new element could be appended, 0 otherwise.
-[[nodiscard("Pointer to new data discared")]] circular *
-circular_append(circular c[const static 1], double value);
+[[nodiscard("Pointer to new data discared"), gnu::nonnull]] circular *
+circular_append(circular *const circle, double value);
 
 // circular_pop: Remove the oldest element from c and return its value.
 // Returns: the removed element if it exists, 0.0 otherwise.
-[[nodiscard]] double circular_pop(circular c[const static 1]);
+[[nodiscard, gnu::nonnull]] double circular_pop(circular *const circle);
 
 // circular_element: Return a pointer to position pos in buffer c.
 // Returns: a pointer to the pos’ element of the buffer, 0 otherwise.
-[[nodiscard]] double *circular_element(size_t pos,
-                                       circular const c[const static 1]);
+[[nodiscard, gnu::nonnull]] double *circular_element(circular *const circle,
+                                                     size_t pos);
+
+// circular_destroy: Destroy circular buffer c.
+// c must have been initialized with a call to `circular_init`
+[[gnu::nonnull]] void circular_destroy(circular *circle);
 
 // circular_init: Initialize a circular buffer c with maximally max_len
 // elements. Only use this function on an uninitialized buffer. Each buffer that
 // is initialized with this function must be destroyed with a call to
 // `circular_destroy`.
-[[nodiscard]] circular *circular_init(circular c[static 1], size_t max_len);
-
-// circular_destroy: Destroy circular buffer c.
-// c must have been initialized with a call to `circular_init`
-void circular_destroy(circular c[static 1]);
-// circular_new: Allocate and initialize a circular buffer with maximally len
-// elements. Each buffer that is allocated with this function must be deleted
-// with a call to circular_delete.
-[[nodiscard, gnu::malloc, gnu::malloc(circular_destroy, 1)]] circular *
-circular_new(size_t len);
+[[nodiscard, gnu::nonnull]] circular *circular_init(circular *circle,
+                                                    size_t max_len);
 
 // circular_delete: Delete circular buffer c.
-// c must have been allocated with a call to circular_new
-void circular_delete(circular c[static 1]);
+// c must have been allocated with a call to `circular_new`
+#if defined(__clang__)
+[[clang::ownership_takes(malloc, 1)]]
+#endif
+[[gnu::nonnull]] void
+circular_delete(circular *circle);
+// circular_new: Allocate and initialize a circular buffer with maximally len
+// elements. Each buffer that is allocated with this function must be deleted
+// with a call to `circular_delete`.
+#if defined(__clang__)
+// https://github.com/llvm/llvm-project/blob/deafb36f87a3541715854d4a620a4cfd6b1ac672/clang/test/Analysis/malloc-annotations.c
+// https://github.com/llvm/llvm-project/issues/76861
+//  1 refers to the first parameter of init which is the size of the malloc .ie
+//  `len` parma
+[[clang::ownership_returns(malloc, 1)]]
+#elif defined(__GNUC__)
+[[gnu::malloc(circular_delete, 1)]]
+#elif defined(_MSC_VER)
+#endif
+[[nodiscard, gnu::malloc]] circular *
+circular_new(size_t len);
 
 // circular_resize: Resize to capacity max_len.
-circular *circular_resize(circular c[const static 1], size_t max_len);
+[[nodiscard("returned  pointer  replaces  function  argument"),
+  gnu::nonnull]] circular *
+circular_resize(circular *circle, size_t new_max_len);
 
 // circular_getlength: Return the number of elements stored.
-size_t circular_getlength(circular const c[const static 1]);
+[[gnu::nonnull]] size_t circular_getlength(circular const *const circle);
+
+#endif // DYNAMIC_MEMORY_H

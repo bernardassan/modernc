c0d71f6bbde90e480fdb0f1ec27bc2fe7f5f6bd7
Ultra-Code <mega.alpha100@gmail.com>
2024-04-30 15:54:38 +0000

    Links to learning on creating type safe template apis in C

---
+++ src/templates.h
@@ -0,0 +1,8 @@
+// INFO: https://www.davidpriver.com/ctemplates.html
+// INFO: https://godbolt.org/z/WKEMMbnzK
+// INFO: STC uses this concept thoroughly https://github.com/stclib/STC
+
+#ifndef TEMPLATES_H
+#define TEMPLATES_H
+
+#endif // TEMPLATES_H

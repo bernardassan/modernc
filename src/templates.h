832efe4c5647ea4dacfdaa4f60a74183c5e6ffd4
Ultra-Code <mega.alpha100@gmail.com>
2024-05-02 17:28:13 +0000

    [template]: improved templates using C23

--- src/templates.h
+++ src/templates.h
@@ -1,8 +1,32 @@
-// INFO: https://www.davidpriver.com/ctemplates.html
-// INFO: https://godbolt.org/z/WKEMMbnzK
-// INFO: STC uses this concept thoroughly https://github.com/stclib/STC
-
 #ifndef TEMPLATES_H
 #define TEMPLATES_H
+
+#include <stdlib.h>
+
+// Common.h
+#define _cat(a, b) a##b
+#define _cat2(a, b) _cat(a, b)
+#define MEMB(name) _cat2(Self, name)
+#ifndef i_tag
+#define i_tag i_val
+#endif
+
+// Vec.h
+#define Self _cat2(Vec_, i_tag)
+
+typedef i_val MEMB(_val);
+typedef struct {
+  MEMB(_val) * at;
+  size_t len, cap;
+} Self;
+
+static inline void MEMB(_push)(Self *self, MEMB(_val) v) {
+  if (self->len == self->cap)
+    self->at = realloc(self->at, (self->cap = self->len * 3 / 2 + 4));
+  self->at[self->len++] = v;
+}
+#undef i_tag
+#undef i_val
+#undef Self
 
 #endif // TEMPLATES_H
